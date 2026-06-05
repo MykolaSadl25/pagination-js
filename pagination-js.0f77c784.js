8bd30945192391c9fbc373866385ca7ac256f7af
Mykola Sadlii <mykolac08@gmail.com>
2026-06-05 17:32:08 +0100

    Updates

--- pagination-js.0f77c784.js
+++ pagination-js.0f77c784.js
@@ -714,383 +714,124 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"2R06K":[function(require,module,exports,__globalThis) {
-var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
-var _lodashDebounce = require("lodash.debounce");
-var _lodashDebounceDefault = parcelHelpers.interopDefault(_lodashDebounce);
-const URL = "https://pixabay.com/api/";
-const API_KEY = "55978698-0d602613e63391cce9d7defd1";
-let currentPage = 1;
-const limit = 9;
-let search = "";
-const listRef = document.querySelector(".list");
-const btnLoadRef = document.querySelector(".button");
-const inputRef = document.querySelector(".searchInput");
-function getImages() {
-    return fetch(`${URL}?key=${API_KEY}&q=${search}&image_type=photo&page=${currentPage}&per_page=${limit}&orientation=horizontal`).then((res)=>res.json());
-}
-function render() {
-    getImages().then((res)=>{
-        if (currentPage * limit >= res.totalHits) {
-            btnLoadRef.disabled = true;
-            btnLoadRef.textContent = "No more images to show";
-            btnLoadRef.classList.add("disabled-btn");
-        }
-        createItemsMarkup(res.hits);
-    });
-}
-function createItemsMarkup(array) {
-    const item = array.map(({ largeImageURL, views, downloads, likes, comments, tags, name })=>{
-        return `<li class="item">
-  <img src="${largeImageURL}" alt="${tags}" class="image">
-  <h2 class="title">${name}</h2>
-  <ul class="stats">
-    <li class="elem">
-       <p class="views">Views:${views}</p>
-    </li>
-    <li class="elem">
-       <p class="downloads">Downloads:${downloads}</p>
-    </li>
-    <li class="elem">
-       <p class="likes">Likes:${likes}</p>
-    </li>
-    <li class="elem">
-       <p class="comments">Comments:${comments}</p>
-    </li>
-  </ul>
-</li>`;
-    }).join("");
-    listRef.insertAdjacentHTML("beforeend", item);
-}
-btnLoadRef.addEventListener("click", ()=>{
-    currentPage += 1;
-    console.log(currentPage);
-    render();
-});
-inputRef.addEventListener("input", (0, _lodashDebounceDefault.default)((e)=>{
-    search = e.target.value;
-    if (search) btnLoadRef.style.display = "block";
-    else if (search === "") {
-        currentPage = 1;
-        listRef.innerHTML = "";
-    }
-    render();
-}, 500));
-btnLoadRef.style.display = "none";
-
-},{"lodash.debounce":"irvaP","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"irvaP":[function(require,module,exports,__globalThis) {
-/**
- * lodash (Custom Build) <https://lodash.com/>
- * Build: `lodash modularize exports="npm" -o ./`
- * Copyright jQuery Foundation and other contributors <https://jquery.org/>
- * Released under MIT license <https://lodash.com/license>
- * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
- * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- */ /** Used as the `TypeError` message for "Functions" methods. */ var global = arguments[3];
-var FUNC_ERROR_TEXT = 'Expected a function';
-/** Used as references for various `Number` constants. */ var NAN = 0 / 0;
-/** `Object#toString` result references. */ var symbolTag = '[object Symbol]';
-/** Used to match leading and trailing whitespace. */ var reTrim = /^\s+|\s+$/g;
-/** Used to detect bad signed hexadecimal string values. */ var reIsBadHex = /^[-+]0x[0-9a-f]+$/i;
-/** Used to detect binary string values. */ var reIsBinary = /^0b[01]+$/i;
-/** Used to detect octal string values. */ var reIsOctal = /^0o[0-7]+$/i;
-/** Built-in method references without a dependency on `root`. */ var freeParseInt = parseInt;
-/** Detect free variable `global` from Node.js. */ var freeGlobal = typeof global == 'object' && global && global.Object === Object && global;
-/** Detect free variable `self`. */ var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
-/** Used as a reference to the global object. */ var root = freeGlobal || freeSelf || Function('return this')();
-/** Used for built-in method references. */ var objectProto = Object.prototype;
-/**
- * Used to resolve the
- * [`toStringTag`](http://ecma-international.org/ecma-262/7.0/#sec-object.prototype.tostring)
- * of values.
- */ var objectToString = objectProto.toString;
-/* Built-in method references for those with the same name as other `lodash` methods. */ var nativeMax = Math.max, nativeMin = Math.min;
-/**
- * Gets the timestamp of the number of milliseconds that have elapsed since
- * the Unix epoch (1 January 1970 00:00:00 UTC).
- *
- * @static
- * @memberOf _
- * @since 2.4.0
- * @category Date
- * @returns {number} Returns the timestamp.
- * @example
- *
- * _.defer(function(stamp) {
- *   console.log(_.now() - stamp);
- * }, _.now());
- * // => Logs the number of milliseconds it took for the deferred invocation.
- */ var now = function() {
-    return root.Date.now();
-};
-/**
- * Creates a debounced function that delays invoking `func` until after `wait`
- * milliseconds have elapsed since the last time the debounced function was
- * invoked. The debounced function comes with a `cancel` method to cancel
- * delayed `func` invocations and a `flush` method to immediately invoke them.
- * Provide `options` to indicate whether `func` should be invoked on the
- * leading and/or trailing edge of the `wait` timeout. The `func` is invoked
- * with the last arguments provided to the debounced function. Subsequent
- * calls to the debounced function return the result of the last `func`
- * invocation.
- *
- * **Note:** If `leading` and `trailing` options are `true`, `func` is
- * invoked on the trailing edge of the timeout only if the debounced function
- * is invoked more than once during the `wait` timeout.
- *
- * If `wait` is `0` and `leading` is `false`, `func` invocation is deferred
- * until to the next tick, similar to `setTimeout` with a timeout of `0`.
- *
- * See [David Corbacho's article](https://css-tricks.com/debouncing-throttling-explained-examples/)
- * for details over the differences between `_.debounce` and `_.throttle`.
- *
- * @static
- * @memberOf _
- * @since 0.1.0
- * @category Function
- * @param {Function} func The function to debounce.
- * @param {number} [wait=0] The number of milliseconds to delay.
- * @param {Object} [options={}] The options object.
- * @param {boolean} [options.leading=false]
- *  Specify invoking on the leading edge of the timeout.
- * @param {number} [options.maxWait]
- *  The maximum time `func` is allowed to be delayed before it's invoked.
- * @param {boolean} [options.trailing=true]
- *  Specify invoking on the trailing edge of the timeout.
- * @returns {Function} Returns the new debounced function.
- * @example
- *
- * // Avoid costly calculations while the window size is in flux.
- * jQuery(window).on('resize', _.debounce(calculateLayout, 150));
- *
- * // Invoke `sendMail` when clicked, debouncing subsequent calls.
- * jQuery(element).on('click', _.debounce(sendMail, 300, {
- *   'leading': true,
- *   'trailing': false
- * }));
- *
- * // Ensure `batchLog` is invoked once after 1 second of debounced calls.
- * var debounced = _.debounce(batchLog, 250, { 'maxWait': 1000 });
- * var source = new EventSource('/stream');
- * jQuery(source).on('message', debounced);
- *
- * // Cancel the trailing debounced invocation.
- * jQuery(window).on('popstate', debounced.cancel);
- */ function debounce(func, wait, options) {
-    var lastArgs, lastThis, maxWait, result, timerId, lastCallTime, lastInvokeTime = 0, leading = false, maxing = false, trailing = true;
-    if (typeof func != 'function') throw new TypeError(FUNC_ERROR_TEXT);
-    wait = toNumber(wait) || 0;
-    if (isObject(options)) {
-        leading = !!options.leading;
-        maxing = 'maxWait' in options;
-        maxWait = maxing ? nativeMax(toNumber(options.maxWait) || 0, wait) : maxWait;
-        trailing = 'trailing' in options ? !!options.trailing : trailing;
-    }
-    function invokeFunc(time) {
-        var args = lastArgs, thisArg = lastThis;
-        lastArgs = lastThis = undefined;
-        lastInvokeTime = time;
-        result = func.apply(thisArg, args);
-        return result;
-    }
-    function leadingEdge(time) {
-        // Reset any `maxWait` timer.
-        lastInvokeTime = time;
-        // Start the timer for the trailing edge.
-        timerId = setTimeout(timerExpired, wait);
-        // Invoke the leading edge.
-        return leading ? invokeFunc(time) : result;
-    }
-    function remainingWait(time) {
-        var timeSinceLastCall = time - lastCallTime, timeSinceLastInvoke = time - lastInvokeTime, result = wait - timeSinceLastCall;
-        return maxing ? nativeMin(result, maxWait - timeSinceLastInvoke) : result;
-    }
-    function shouldInvoke(time) {
-        var timeSinceLastCall = time - lastCallTime, timeSinceLastInvoke = time - lastInvokeTime;
-        // Either this is the first call, activity has stopped and we're at the
-        // trailing edge, the system time has gone backwards and we're treating
-        // it as the trailing edge, or we've hit the `maxWait` limit.
-        return lastCallTime === undefined || timeSinceLastCall >= wait || timeSinceLastCall < 0 || maxing && timeSinceLastInvoke >= maxWait;
-    }
-    function timerExpired() {
-        var time = now();
-        if (shouldInvoke(time)) return trailingEdge(time);
-        // Restart the timer.
-        timerId = setTimeout(timerExpired, remainingWait(time));
-    }
-    function trailingEdge(time) {
-        timerId = undefined;
-        // Only invoke if we have `lastArgs` which means `func` has been
-        // debounced at least once.
-        if (trailing && lastArgs) return invokeFunc(time);
-        lastArgs = lastThis = undefined;
-        return result;
-    }
-    function cancel() {
-        if (timerId !== undefined) clearTimeout(timerId);
-        lastInvokeTime = 0;
-        lastArgs = lastCallTime = lastThis = timerId = undefined;
-    }
-    function flush() {
-        return timerId === undefined ? result : trailingEdge(now());
-    }
-    function debounced() {
-        var time = now(), isInvoking = shouldInvoke(time);
-        lastArgs = arguments;
-        lastThis = this;
-        lastCallTime = time;
-        if (isInvoking) {
-            if (timerId === undefined) return leadingEdge(lastCallTime);
-            if (maxing) {
-                // Handle invocations in a tight loop.
-                timerId = setTimeout(timerExpired, wait);
-                return invokeFunc(lastCallTime);
-            }
-        }
-        if (timerId === undefined) timerId = setTimeout(timerExpired, wait);
-        return result;
-    }
-    debounced.cancel = cancel;
-    debounced.flush = flush;
-    return debounced;
-}
-/**
- * Checks if `value` is the
- * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
- * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
- *
- * @static
- * @memberOf _
- * @since 0.1.0
- * @category Lang
- * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is an object, else `false`.
- * @example
- *
- * _.isObject({});
- * // => true
- *
- * _.isObject([1, 2, 3]);
- * // => true
- *
- * _.isObject(_.noop);
- * // => true
- *
- * _.isObject(null);
- * // => false
- */ function isObject(value) {
-    var type = typeof value;
-    return !!value && (type == 'object' || type == 'function');
-}
-/**
- * Checks if `value` is object-like. A value is object-like if it's not `null`
- * and has a `typeof` result of "object".
- *
- * @static
- * @memberOf _
- * @since 4.0.0
- * @category Lang
- * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is object-like, else `false`.
- * @example
- *
- * _.isObjectLike({});
- * // => true
- *
- * _.isObjectLike([1, 2, 3]);
- * // => true
- *
- * _.isObjectLike(_.noop);
- * // => false
- *
- * _.isObjectLike(null);
- * // => false
- */ function isObjectLike(value) {
-    return !!value && typeof value == 'object';
-}
-/**
- * Checks if `value` is classified as a `Symbol` primitive or object.
- *
- * @static
- * @memberOf _
- * @since 4.0.0
- * @category Lang
- * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is a symbol, else `false`.
- * @example
- *
- * _.isSymbol(Symbol.iterator);
- * // => true
- *
- * _.isSymbol('abc');
- * // => false
- */ function isSymbol(value) {
-    return typeof value == 'symbol' || isObjectLike(value) && objectToString.call(value) == symbolTag;
-}
-/**
- * Converts `value` to a number.
- *
- * @static
- * @memberOf _
- * @since 4.0.0
- * @category Lang
- * @param {*} value The value to process.
- * @returns {number} Returns the number.
- * @example
- *
- * _.toNumber(3.2);
- * // => 3.2
- *
- * _.toNumber(Number.MIN_VALUE);
- * // => 5e-324
- *
- * _.toNumber(Infinity);
- * // => Infinity
- *
- * _.toNumber('3.2');
- * // => 3.2
- */ function toNumber(value) {
-    if (typeof value == 'number') return value;
-    if (isSymbol(value)) return NAN;
-    if (isObject(value)) {
-        var other = typeof value.valueOf == 'function' ? value.valueOf() : value;
-        value = isObject(other) ? other + '' : other;
-    }
-    if (typeof value != 'string') return value === 0 ? value : +value;
-    value = value.replace(reTrim, '');
-    var isBinary = reIsBinary.test(value);
-    return isBinary || reIsOctal.test(value) ? freeParseInt(value.slice(2), isBinary ? 2 : 8) : reIsBadHex.test(value) ? NAN : +value;
-}
-module.exports = debounce;
-
-},{}],"jnFvT":[function(require,module,exports,__globalThis) {
-exports.interopDefault = function(a) {
-    return a && a.__esModule ? a : {
-        default: a
-    };
-};
-exports.defineInteropFlag = function(a) {
-    Object.defineProperty(a, '__esModule', {
-        value: true
-    });
-};
-exports.exportAll = function(source, dest) {
-    Object.keys(source).forEach(function(key) {
-        if (key === 'default' || key === '__esModule' || Object.prototype.hasOwnProperty.call(dest, key)) return;
-        Object.defineProperty(dest, key, {
-            enumerable: true,
-            get: function() {
-                return source[key];
-            }
-        });
-    });
-    return dest;
-};
-exports.export = function(dest, destName, get) {
-    Object.defineProperty(dest, destName, {
-        enumerable: true,
-        get: get
-    });
-};
+// import debounce from "lodash.debounce";
+// const URL = "https://pixabay.com/api/";
+// const API_KEY = "55978698-0d602613e63391cce9d7defd1";
+// let currentPage = 1;
+// const limit = 9;
+// let search = "";
+// const listRef = document.querySelector(".list");
+// const btnLoadRef = document.querySelector(".button");
+// const inputRef = document.querySelector(".searchInput");
+// function getImages() {
+//     return fetch(`${URL}?key=${API_KEY}&q=${search}&image_type=photo&page=${currentPage}&per_page=${limit}&orientation=horizontal`).then(res=>res.json());
+// }
+// function render() {
+//     getImages().then(res=>{
+//         if (currentPage*limit >= res.totalHits) {
+//             btnLoadRef.disabled=true;
+//             btnLoadRef.textContent="No more images to show";
+//             btnLoadRef.classList.add("disabled-btn")
+//         }
+//         createItemsMarkup(res.hits)
+//     })
+// }
+// function createItemsMarkup(array) {
+//     const item = array.map(({largeImageURL,views,downloads,likes,comments,tags,name})=>{
+//         return `<li class="item">
+//   <img src="${largeImageURL}" alt="${tags}" class="image">
+//   <h2 class="title">${name}</h2>
+//   <ul class="stats">
+//     <li class="elem">
+//        <p class="views">Views:${views}</p>
+//     </li>
+//     <li class="elem">
+//        <p class="downloads">Downloads:${downloads}</p>
+//     </li>
+//     <li class="elem">
+//        <p class="likes">Likes:${likes}</p>
+//     </li>
+//     <li class="elem">
+//        <p class="comments">Comments:${comments}</p>
+//     </li>
+//   </ul>
+// </li>`
+//     }).join("");
+//     listRef.insertAdjacentHTML("beforeend",item)
+// }
+// btnLoadRef.addEventListener("click",()=>{
+//     currentPage+=1;
+//     console.log(currentPage);
+//     render()
+// });
+// inputRef.addEventListener("input",debounce((e)=>{
+//     search = e.target.value;
+//     if (search) {
+//         btnLoadRef.style.display="block"
+//     }
+//     else if(search==="") {
+//         currentPage=1
+//         listRef.innerHTML=""
+//     }
+//     render()
+// },500))
+// btnLoadRef.style.display="none"
+// import debounce from "lodash.debounce";
+// import * as basicLightbox from 'basiclightbox';
+// import 'basiclightbox/dist/basicLightbox.min.css';
+// const URL = "https://pixabay.com/api/";
+// const API_KEY = "55978698-0d602613e63391cce9d7defd1";
+// let currentPage = 1;
+// const limit = 12;
+// let search = "";
+// let largeImage = null;
+// let instance = null;
+// const inputRef = document.querySelector(".input");
+// const listRef = document.querySelector(".list");
+// const element = document.querySelector(".element");
+// inputRef.addEventListener("input",debounce((e)=>{
+//     listRef.innerHTML="";
+//     search=""
+//     search = e.target.value.trim();
+//     getImages().then(res=>createImages(res.hits));
+// },500))
+// function getImages() {
+//     return fetch(`${URL}?key=${API_KEY}&q=${search}&image_type=photo&page=${currentPage}&per_page=${limit}&orientation=horizontal`).then(res=>res.json());
+// }
+// function createImages(array) {
+//     const image = array.map(({previewURL,largeImageURL,tags})=>{
+//         return `<li class="item" data-action="${largeImageURL}">
+//    <img src="${previewURL}" alt="${tags}" class="image">
+//  </li>`
+//     }).join("");
+//     listRef.insertAdjacentHTML("beforeend",image);
+// }
+// listRef.addEventListener("click",(e)=>{
+//     if (e.target.nodeName !== "IMG") {
+//         return;
+//     }
+//     largeImage = e.target.closest("li").dataset.action;
+//     instance = basicLightbox.create(`
+//     <div class="modal">
+//         <img src="${largeImage}" alt="photo" class="image">
+//     </div>
+// `);
+// instance.show()
+// })
+// window.addEventListener("keydown",(e)=>{
+//     if (e.code==="Escape") {
+//         instance.close()
+//     }
+// })
+// const observer = new IntersectionObserver(([entries]) =>{
+//     if (entries.isIntersecting  && inputRef.value !== "") {
+//         currentPage += 1
+//         getImages().then(res=>createImages(res.hits));
+//     }
+// },{
+//     rootMargin: "50px",
+// })
+// observer.observe(element)
 
 },{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequirecb64", {})
 
